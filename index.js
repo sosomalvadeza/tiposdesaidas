06dc6312910c4f0b24168f80fa600c55088bf7c3
sosomalvadeza <162622311+sosomalvadeza@users.noreply.github.com>
2025-09-18 18:21:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+document
+    .getElementById("texto").innerHTML=
+    "Meu primeiro texto <b>JS - Sofia</b>!";
+
+alert(10 + 5);
+console.log("Oi isso é um console log");
+let a,b,c;
+a=5;
+b=6;
+c=7;
+let pessoa = "Dimitri";
+let ="Dimitri";
+function minhafuncao(){
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+}
